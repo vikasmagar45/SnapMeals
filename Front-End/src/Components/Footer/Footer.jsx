91a63dc310adbc48fde136322173ad5fb7d86f72
Vikas Magar <108778635+vikasmagar45@users.noreply.github.com>
2024-09-28 16:52:39 +0530

    Update Footer.jsx

--- Front-End/src/Components/Footer/Footer.jsx
+++ Front-End/src/Components/Footer/Footer.jsx
@@ -13,10 +13,28 @@ const Footer = () => {
             maiores illum, quis explicabo vel facilis voluptates, voluptatem,
             earum officia eveniet repellat.
           </p>
-          <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-            <img src={assets.linkedin_icon} alt="" />
+           <div className="footer-social-icons">
+            <a
+              href="https://facebook.com"
+              className="hover:text-gray-400"
+              aria-label="Facebook"
+            >
+              <img src={assets.facebook_icon} alt="" />
+            </a>
+            <a
+              href="https://twitter.com"
+              className="hover:text-gray-400"
+              aria-label="Twitter"
+            >
+              <img src={assets.twitter_icon} alt="" />
+            </a>
+            <a
+              href="https://linkedin.com"
+              className="hover:text-gray-400"
+              aria-label="LinkedIn"
+            >
+              <img src={assets.linkedin_icon} alt="" />
+            </a>
           </div>
         </div>
         <div className="footer-content-center">
