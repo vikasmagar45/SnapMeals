4d0d3be2a188b815a39f1c46240c6e8e92a350d3
Vikas Magar <108778635+vikasmagar45@users.noreply.github.com>
2024-09-28 17:17:07 +0530

    Update Header.jsx

--- Front-End/src/Components/Header/Header.jsx
+++ Front-End/src/Components/Header/Header.jsx
@@ -7,7 +7,13 @@ const Header = () => {
         <div className="header-contents">
             <h2>Order your favorite food</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-            <button>View Menu</button>
+            <a
+          href="#explore-menu"
+          onClick={() => setMenu('Menu')}
+          className={menu === 'Menu' ? 'active' : ''}
+        >
+          <button className='btn'>View Menu</button>
+        </a>
         </div>
     </div>
   )
